8c7b111da861e17f56367bd0466d84e7713af41f
asmaelkb <asmaekd28@gmail.com>
2024-10-02 09:08:59 +0200

    Modif send et receive (offset mal actualisé)

--- Task2/src/implementation/MessageQueue.java
+++ Task2/src/implementation/MessageQueue.java
@@ -13,25 +13,22 @@ public class MessageQueue {
 	}
 	
 	public void send(byte[] bytes, int offset, int length) throws DisconnectedException {
-		byte[] lengthMsg = ByteBuffer.allocate(4).putInt(length).array();
-		channel.write(lengthMsg, offset, 4);
-		offset += 4;
-		channel.write(bytes, offset, length);
+		byte[] msgLength = ByteBuffer.allocate(4).putInt(length).array();
+	    channel.write(msgLength, 0, msgLength.length); 
+	    channel.write(bytes, offset, length);
 	}
-	
+
 	public byte[] receive() throws DisconnectedException {
-		int offset = channel.out.m_head;
-		
-		byte[] lengthMsg = new byte[4];
-		channel.read(lengthMsg, offset, 4);
-		offset += 4;
-		
-		int length = ByteBuffer.wrap(lengthMsg).getInt();
-		byte[] msg = new byte[length];
-		channel.read(lengthMsg, offset, length);
-		
-		return msg;
+		byte[] msgLength = new byte[4];
+	    channel.read(msgLength, 0, 4);
+	    int length = ByteBuffer.wrap(msgLength).getInt();
+
+	    byte[] msg = new byte[length];
+	    channel.read(msg, 0, length);
+
+	    return msg;
 	}
+
 	
 	void close() {
 		channel.disconnect();
